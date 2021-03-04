9934ce47b98c68e033f8e054c3fa448edde57f44
AWilson <dreamy26@icloud.com>
2021-03-04 16:47:37 -0500

    added Api's needed to begin application build.

---
+++ PlanFort/PlanFort/Services/SeetGeakClients.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PlanFort.Services
+{
+    public class SeetGeakClients
+    {
+        public SeetGeakClients()
+
+        {
+            public SeetGeakClients (HttpClient client)
+            {
+                _client = client;
+            }
+        }
+    }
+}
