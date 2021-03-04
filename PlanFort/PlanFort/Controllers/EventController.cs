89cbed51524bffd4a64c8aa7268fc19ab04dbfe3
Tsavage1994 <theresa.savage98@yahoo.com>
2021-03-03 22:26:05 -0500

    Added APIs

---
+++ PlanFort/PlanFort/Controllers/EventController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlanFort.Models.DomainModels;
+using PlanFort.Models.ViewModels;
+using PlanFort.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlanFort.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly SeatGeekClient _seatGeekClient;
+        private readonly YelpClient _yelpClient;
+
+        public EventController(SeatGeekClient seatGeekClient, YelpClient yelpClient)//<< using dependency injection
+        {
+            _seatGeekClient = seatGeekClient;
+            _yelpClient = yelpClient;
+        }
+        public IActionResult EventCityForm()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> EventCityFormResult(EventCityFormVM model)
+        {
+            var response = await _seatGeekClient.GetEventByCity(model.City);
+
+            var viewModel = new EventCityFormResultVM();
+            var results = response.events;
+
+            viewModel.Events = results
+                .Select(results => new EventVM()
+                { City = results.venue.city, Title = results.title })
+                .ToList();
+
+            return View(viewModel);
+        }
+        public IActionResult BusinessForm()
+        {
+            return View();
+        }
+        public async Task<IActionResult> BusinessFormResult(BusinessFormVM model)
+        {
+            var response = await _yelpClient.GetBusinessByCity(model.City, model.Category);
+
+            var viewModel = new BusinessFormResultVM();
+            var results = response.businesses;
+
+            viewModel.Businesses = results
+                .Select(results => new BusinessVM()
+                { Name = results.name })
+                .ToList();
+
+            return View(viewModel);
+        }
+
+    }
+}
---
+++ PlanFort/PlanFort/Models/DomainModels/BusinessVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlanFort.Models.DomainModels
+{
+    public class BusinessVM
+    {
+        public string Name { get; set; }
+    }
+}
