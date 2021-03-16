6c0a0f8499a5c9cdedfd60287fbe482ba7685c5c
Tsavage1994 <theresa.savage98@yahoo.com>
2021-03-15 20:53:59 -0400

    Offical Weather API

---
+++ PlanFort/PlanFort/Migrations/20210316005208_AddedDates.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlanFort.Migrations
+{
+    public partial class AddedDates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ PlanFort/PlanFort/Services/OpenWeatherClient.cs
@@ -0,0 +1,38 @@
+﻿using PlanFort.Models.APIModels.OpenWeatherAPIModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PlanFort.Services
+{
+    public class OpenWeatherClient
+    {
+        private readonly HttpClient _openWeatherClient;
+
+        public OpenWeatherClient(HttpClient openWeatherClient)
+        {
+            _openWeatherClient = openWeatherClient;
+        }
+
+        public async Task<WeatherResponseModel> GetWeather(string city)
+        {
+            var response = await _openWeatherClient.GetAsync($"data/2.5/weather?q={city}&units=imperial&appid=26a6a596dfa6ff3649d805a3cf7dbc34");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStreamAsync();
+
+                var model = await JsonSerializer.DeserializeAsync<WeatherResponseModel>(content);
+
+                return model;
+            }
+            else
+            {
+                throw new HttpRequestException("Open Weather API bad resposne");
+            }
+        }
+    }
+}
